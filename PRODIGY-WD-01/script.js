c247cae9e101c49cd1e8b37fef7eedf9279cd5a3
imisgsc <simigracia@gmail.com>
2024-06-16 08:50:26 +0530

    editing script.js

--- PRODIGY-WD-01/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {
-        navbar.style.backgroundColor = '#222'; // Change background color when scrolled
-    } else {
-        navbar.style.backgroundColor = '#333'; // Default background color
-    }
-});
